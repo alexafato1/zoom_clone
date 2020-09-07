4523db9fcb138efe3d901922230018c99f4f81f2
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-09-07 20:11:00 +0300

    2.11

--- public/script.js
+++ public/script.js
@@ -6,31 +6,50 @@ var peer = new Peer(undefined, {
     host: '/',
     port: '3000'
 });
+
+
 let myVideoStream
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream)
+
+   peer.on('call', call => {
+       call.answer(stream)
+       const video = document.createElement('video')
+       call.on('stream', userVideoStream => {
+           addVideoStream(video, userVideoStream)
+       })
+   })
+
+   socket.on('user-connected', (userId) => {
+       connectToNewUser(userId, stream);
+   })
 })
+
+ 
 
 peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);  
 })
 
 
+const connectToNewUser = (userId, stream) => {  
+  const call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
 
-socket.on('user-connected', () => {
-    connectToNewUser();
-})
-
-const connectToNewUser = () => {
-  console.log('new user');
+ 
+  })
 }
+
+
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
